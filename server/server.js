db38c67c09c69848ef58f970c6334f46b23a9039
Mike Cossalter <mikecossalter@yahoo.com>
2020-07-21 11:33:21 -0500

    deploy test with port issue fixed

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ app.use(express.static('server/public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 //globals
-const port = 5000;
+const port = process.env.PORT || 5000;
 let entries = [];
 let solvedProblem = [];
 
